1c511b48fa94f345393194583a03ca9838de7f8e
hengwu0 <wu.heng@zte.com.cn>
2020-09-18 09:56:40 +0800

    修复日志输出路径错误

--- src/Algorithm/alog.go
+++ src/Algorithm/alog.go
@@ -4,20 +4,22 @@ package algorithm
 import (
 	"log"
 	"os"
+	"path"
 	"time"
 )
 
 var file *os.File
 var logger *log.Logger
-var logdir = "./log/"
+var logdir = path.Join(path.Dir(os.Args[0]), "./log/") + "/"
 
 func init() {
 	var err error
+
 	if err = os.MkdirAll(logdir, 0755); err != nil {
 		log.Fatalln(`fail to create dir: "./log"! %v`, err)
 	}
 	filename := logdir + "ClipBoard_" + time.Now().Format("2006-01-02") + ".log"
-	if file, err = os.Create(filename); err != nil {
+	if file, err = os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err != nil {
 		log.Fatalln("fail to create %s file! %v", filename, err)
 	}
 	logger = log.New(file, "", log.LstdFlags) //log.Lshortfile
@@ -30,7 +32,7 @@ func Close() {
 func (alist *Alist) GetLogger() *log.Logger {
 	if alist.logger == nil {
 		filename := logdir + alist.hash + "_" + time.Now().Format("2006-01-02") + ".log"
-		if f, err := os.Create(filename); err == nil {
+		if f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644); err == nil {
 			alist.file = f
 			alist.logger = log.New(alist.file, "", log.LstdFlags)
 			return alist.logger
